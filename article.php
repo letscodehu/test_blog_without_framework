214c8e76fef680a0ca5df03fd5e86293db8a873c
timeavass <timea.vass.mail@gmail.com>
2017-10-23 21:39:56 +0200

    minor changes in query: old:heredoc, new, classic sql

--- article.php
+++ article.php
@@ -1,6 +1,18 @@
 <?php
 
-//nastavenie toho, ze ktoru stranku require v @layout.html.php: *.html.php
-if (isset($_GET['page'])){
-    $selectedPage = "article";
-}
+require "master.inc.php";
+
+
+//query for choosing first 3 posts
+$sql = "SELECT * FROM post ORDER BY created_at";
+
+//writing out all posts - homepage
+$posts = $database->query($sql)->fetch();
+
+
+//which page will be required in @layout.html.php. format: $page.html.php
+$page = "home";
+$pageTitle = "Home";
+
+
+include "templates/@layout.html.php";
