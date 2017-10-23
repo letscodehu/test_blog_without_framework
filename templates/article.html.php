8726276a3f27a1b715790a729bf4ab5339cf1081
timeavass <timea.vass.mail@gmail.com>
2017-10-23 22:46:51 +0200

    wip

--- templates/article.html.php
+++ templates/article.html.php
@@ -1,9 +1,13 @@
 
 <h2><?php echo $selectedPost->title ?></h2>
 
-<p><?php echo $posts->content ?></p>
+<p><?php echo $selectedPost->content ?></p>
 
 <?php
-foreach $comments as $comment?>
-<p><?php echo $comment->created_by ?></p>
-<p><?php echo $comment->comment ?></p>
+foreach($comments as $comment){ ?>
+    <br>
+    <p class= "post-meta" style="color:darkgrey"><?php echo $comment->created_by ?></p>
+    <p><?php echo $comment->comment ?></p>
+
+<?php
+}
